b422a8d480fe5647720a7beb58a8d920e21f16b8
alvalea <alvalea@gmail.com>
2020-07-23 15:53:36 +0000

    Create mongo module for the server

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
+	"github.com/alvalea/wapp/mongo"
 	"github.com/alvalea/wapp/server"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/websocket"
 	"html/template"
 	"log"
@@ -14,72 +11,6 @@ import (
 	"net/rpc/jsonrpc"
 	"os"
 )
-
-type mongoDatabase struct {
-	collection *mongo.Collection
-}
-
-func (m *mongoDatabase) Find(args *server.Args, res *server.Result) error {
-	skip := int64(0)
-	if args.Page > 0 {
-		skip = int64((args.Page - 1) * args.PageSize)
-	}
-	limit := int64(args.PageSize)
-
-	opts := options.FindOptions{
-		Skip:  &skip,
-		Limit: &limit,
-	}
-	filter := bson.M{"name": bson.M{"$regex": args.Input + "*"}}
-	cursor, err := m.collection.Find(context.TODO(), filter, &opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err = cursor.All(context.TODO(), &res.Students); err != nil {
-		log.Fatal(err)
-	}
-
-	return err
-}
-
-func (m *mongoDatabase) Search(args *server.SearchArgs, res *server.SearchResult) error {
-	limit := int64(5)
-	opts := options.FindOptions{
-		Projection: bson.D{{"name", 1}},
-		Limit:      &limit,
-	}
-	filter := bson.M{"name": bson.M{"$regex": args.Input + "*"}}
-	cursor, err := m.collection.Find(context.TODO(), filter, &opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err = cursor.All(context.TODO(), &res.Students); err != nil {
-		log.Fatal(err)
-	}
-
-	return err
-}
-
-func dbConnect() *mongo.Collection {
-	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	} else {
-		log.Println("Connected to MongoDB!")
-	}
-
-	return client.Database("sample_school").Collection("students")
-}
 
 func serveWS(server *rpc.Server) http.Handler {
 	return websocket.Handler(func(ws *websocket.Conn) {
@@ -106,8 +37,12 @@ func servePage(t *template.Template) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	collection := dbConnect()
-	mongoDB := &mongoDatabase{collection}
+	mongoDB, err := mongo.NewDB("mongodb://mongo:27017",
+		"sample_school", "students")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	service := &server.Service{mongoDB}
 	wsServer := rpc.NewServer()
---
+++ mongo/mongodb.go
@@ -0,0 +1,72 @@
+package mongo
+
+import (
+	"context"
+	"github.com/alvalea/wapp/server"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+type MongoDatabase struct {
+	collection *mongo.Collection
+}
+
+func (m *MongoDatabase) Find(args *server.Args, res *server.Result) error {
+	skip := int64(0)
+	if args.Page > 0 {
+		skip = int64((args.Page - 1) * args.PageSize)
+	}
+	limit := int64(args.PageSize)
+
+	opts := options.FindOptions{
+		Skip:  &skip,
+		Limit: &limit,
+	}
+	filter := bson.M{"name": bson.M{"$regex": args.Input + "*"}}
+	cursor, err := m.collection.Find(context.TODO(), filter, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = cursor.All(context.TODO(), &res.Students); err != nil {
+		log.Fatal(err)
+	}
+
+	return err
+}
+
+func (m *MongoDatabase) Search(args *server.SearchArgs, res *server.SearchResult) error {
+	limit := int64(5)
+	opts := options.FindOptions{
+		Projection: bson.D{{"name", 1}},
+		Limit:      &limit,
+	}
+	filter := bson.M{"name": bson.M{"$regex": args.Input + "*"}}
+	cursor, err := m.collection.Find(context.TODO(), filter, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = cursor.All(context.TODO(), &res.Students); err != nil {
+		log.Fatal(err)
+	}
+
+	return err
+}
+
+func NewDB(uri string, db string, col string) (*MongoDatabase, error) {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database(db).Collection(col)
+
+	return &MongoDatabase{collection}, err
+}
